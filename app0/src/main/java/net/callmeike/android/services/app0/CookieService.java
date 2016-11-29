4cf88d14c52071d5b839f91fd127c56c5b4c9150
G. Blake Meike <blake.meike@gmail.com>
2016-11-29 09:52:51 -0800

    Demonstrate an AsyncTask Service

--- app0/src/main/java/net/callmeike/android/services/app0/CookieService.java
+++ app0/src/main/java/net/callmeike/android/services/app0/CookieService.java
@@ -18,6 +18,7 @@ package net.callmeike.android.services.app0;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -28,12 +29,41 @@ public class CookieService extends Service {
     private static final String TAG = "COOKIESVC";
 
     private static final String ACTION_EAT = "CookieService.ACTION.EAT";
+    private static final String ACTION_EAT_NOISILY = "CookieService.ACTION.EAT_NOISILY";
     private static final String PARAM_COOKIE = "CookieService.PARAM.COOKIE";
 
+    private class IntentTask extends AsyncTask<Intent, Void, Void> {
+        @Override
+        protected Void doInBackground(Intent... intents) {
+            processIntent(intents[0]);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            stop();
+        }
+
+        @Override
+        protected void onCancelled(Void aVoid) {
+            stop();
+        }
+
+        private void stop() {
+            CookieService.this.stopSelf();
+        }
+    }
 
     public static void eatACookie(@NonNull Context ctxt, @NonNull String cookie) {
         Intent intent = new Intent(ctxt, CookieService.class);
         intent.setAction(ACTION_EAT);
+        intent.putExtra(PARAM_COOKIE, cookie);
+        ctxt.startService(intent);
+    }
+
+    public static void eatACookieNoisily(@NonNull Context ctxt, @NonNull String cookie) {
+        Intent intent = new Intent(ctxt, CookieService.class);
+        intent.setAction(ACTION_EAT_NOISILY);
         intent.putExtra(PARAM_COOKIE, cookie);
         ctxt.startService(intent);
     }
@@ -46,19 +76,40 @@ public class CookieService extends Service {
 
     @Override
     public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
+        new IntentTask().execute(intent);
+        return START_NOT_STICKY;
+    }
+
+    void processIntent(@Nullable Intent intent) {
         String action = intent.getAction();
         switch (action) {
             case ACTION_EAT:
                 doEatACookie(intent.getStringExtra(PARAM_COOKIE));
                 break;
+            case ACTION_EAT_NOISILY:
+                doEatACookieNoisily(intent.getStringExtra(PARAM_COOKIE));
+                break;
             default:
                 Log.w(TAG, "unexpected action: " + action);
         }
-
-        return Service.START_NOT_STICKY;
     }
 
     private void doEatACookie(String cookie) {
-        Log.w(TAG, "munch, munch, munch: " + cookie);
+        Log.w(TAG, "ate: " + cookie);
+        delay(60);
+    }
+
+    private void doEatACookieNoisily(String cookie) {
+        Log.w(TAG, "MUNCH, MUNCH, MUNCH: " + cookie);
+        delay(60);
+    }
+
+    private void delay(long t) {
+        long done = System.currentTimeMillis() + (t * 1000);
+        while (true) {
+            long wait = done - System.currentTimeMillis();
+            try { Thread.sleep(wait); }
+            catch (InterruptedException ignore) {}
+        }
     }
 }
